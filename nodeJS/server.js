9553a7d017f2feaff500db059dcdbc7d5004ac14
JUNSUNG <hjs8213073@gmail.com>
2023-01-09 14:46:36 +0900

    #4 - nodejs 고급
    login 기능 session 생성 기능

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -6,8 +6,13 @@ const app = express(); // 객체 생성
 //ejs 사용하기위한 코드
 app.set('view engine', 'ejs');
 
-//stati 파일을 보관하기위해 public 폴더 사용을 선언
+//static 파일을 보관하기위해 public 폴더 사용을 선언
 app.use('/public', express.static('public'));
+
+//method-override 쓰기 위한 코드
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 
 //db 접속을 위한 변수 생성
 var db;
@@ -100,8 +105,6 @@ app.get('/list', function(요청, 응답){
         응답.render('list.ejs', {posts: 결과});
     });
 
-    
-
 });
 
 app.delete('/delete', function(요청,응답){
@@ -123,3 +126,66 @@ app.get('/detail/:id', function(요청, 응답){
         응답.render('detail.ejs', { data : 결과})
     });
 })
+
+app.get('/edit/:id', function(요청,응답){
+    db.collection('post').findOne({_id: parseInt(요청.params.id)}, function(에러,결과){
+        console.log(결과);
+        응답.render('edit.ejs', {post : 결과});
+
+    })
+})
+
+app.put('/edit', function(요청,응답){
+    //폼에 담긴 제목데이터, 날짜데이터를 가지고 db.collection에다가 업데이트함
+    db.collection('post').updateOne({_id: parseInt(요청.body.id)},{$set: {제목: 요청.body.title, 날짜: 요청.body.date}},function(에러,결과){
+        console.log('수정완료');
+        응답.redirect('/list');
+    })
+});
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+//app.use(미들웨어)
+//미들웨어 : 요청 - 응답 중간에 뭔가 실행되는 코드
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(요청,응답){
+    응답.render('login.ejs');
+})
+
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(요청, 응답){
+    응답.redirect('/')
+  });
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',    // form의 name 속성
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+        //done => done(서버에러, 성공시 사용자 DB데이터, 에러메세지)
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
+
+  //session 등록
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(아이디, done){
+    done(null, {});
+});
